c4356ca2df5f6c294b24c401837f85bf20c2c015
Bao Tran <bao.tran@sstechvn.com>
2023-04-19 10:48:23 +0700

    Create tabs and apply to modal

---
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,31 @@
+import { Component, ContentChildren, AfterContentInit, QueryList } from '@angular/core';
+
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrls: ['./tabs-container.component.css']
+})
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
+
+  constructor() { }
+
+  ngAfterContentInit(): void {
+    const activeTab = this.tabs?.filter(tab => tab.active);
+
+    if (!activeTab || activeTab.length === 0) {
+      this.selectTab(this.tabs!.first);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => {
+      tab.active = false;
+    });
+
+    tab.active = true;
+    return false;
+  }
+}
