2e2c8e80f3d2554bad4bd702ef91fbf562b13833
Bao Tran <bao.tran@sstechvn.com>
2023-04-19 12:09:55 +0700

    User form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from "../shared/shared.module";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AuthModalComponent
+    AuthModalComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   exports: [
     AuthModalComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class UserModule { }
