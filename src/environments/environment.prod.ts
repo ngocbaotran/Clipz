219d693a5e157b3ed923f3ed3c98f6b79033e44c
Bao Tran <bao.tran@sstechvn.com>
2023-04-27 15:12:03 +0700

    Authentication - Importing AngularFire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCaF5NS9nbsPeM8EDry9p9APfMn9k_DgP8",
+    authDomain: "clips-aa469.firebaseapp.com",
+    projectId: "clips-aa469",
+    storageBucket: "clips-aa469.appspot.com",
+    messagingSenderId: "777200319830",
+    appId: "1:777200319830:web:ce874d74d29c0f37c76af7"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCaF5NS9nbsPeM8EDry9p9APfMn9k_DgP8",
+    authDomain: "clips-aa469.firebaseapp.com",
+    projectId: "clips-aa469",
+    storageBucket: "clips-aa469.appspot.com",
+    messagingSenderId: "777200319830",
+    appId: "1:777200319830:web:ce874d74d29c0f37c76af7"
+  }
 };
 
 /*
