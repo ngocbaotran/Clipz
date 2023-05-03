46689e7d0e781f57f2fe3a2da449fec4de55562a
Bao Tran <ngocbao8994@gmail.com>
2023-05-03 23:23:50 +0700

    Authentication - Setting up the Login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,13 +12,37 @@ export class LoginComponent implements OnInit {
     email: '',
     password: ''
   };
+  showAlert = false;
+  alertMsg =  'Please wait! We are logging you in.';
+  alertColor = 'blue';
+  inSubmission = false;
 
-  constructor() { }
+  constructor(
+    private auth: AngularFireAuth
+  ) { }
 
   ngOnInit(): void {
   }
 
-  login() {
-    console.log(this.credentials)
+  async login() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! We are logging you in.';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      );
+    } catch (e) {
+      this.inSubmission = false;
+      this.alertMsg = 'An unexpected error occurred. Please try again later.';
+      this.alertColor = 'red';
+      return;
+    }
+
+    this.alertMsg = 'Success! You are now logged in.';
+    this.alertColor = 'green';
   }
 }
