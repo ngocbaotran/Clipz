c3a2bfba2db7a86f726cc0830405e3ad5b8a8fdf
Bao Tran <ngocbao8994@gmail.com>
2023-05-03 23:36:18 +0700

    Authentication - Destroying the Modal

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnInit} from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
 
 import { ModalService } from '../../services/modal.service';
 
@@ -8,7 +8,7 @@ import { ModalService } from '../../services/modal.service';
   styleUrls: ['./modal.component.css'],
   // providers: [ModalService]
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
   @Input() modalID = '';
 
   constructor(
@@ -20,6 +20,10 @@ export class ModalComponent implements OnInit {
 
   ngOnInit(): void {
     document.body.appendChild(this.el.nativeElement);
+  }
+
+  ngOnDestroy() {
+    document.body.removeChild(this.el.nativeElement);
   }
 
   closeModal() {
