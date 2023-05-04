0676166edb658b32dd91446201723d32a7393570
Bao Tran <bao.tran@sstechvn.com>
2023-05-04 11:07:00 +0700

    Custom Validators - Validating Passwords

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import {AuthService} from "../../services/auth.service";
+
+import { AuthService } from "../../services/auth.service";
+import { RegisterValidators } from '../validators/register-validators';
 
 @Component({
   selector: 'app-register',
@@ -45,7 +47,7 @@ export class RegisterComponent {
     password: this.password,
     confirm_password: this.confirm_password,
     phoneNumber: this.phoneNumber,
-  });
+  }, [RegisterValidators.match('password', 'confirm_password')]);
 
   constructor(
     private auth: AuthService
