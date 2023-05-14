b70dab76589042ba773958dc38b53a1a94579e39
Bao Tran <ngocbao8994@gmail.com>
2023-05-14 23:40:02 +0700

    Uploading Files - Binding the Edit Form

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { ModalService } from '../../services/modal.service';
 import IClip from '../../models/clip.model';
@@ -8,8 +9,18 @@ import IClip from '../../models/clip.model';
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit, OnDestroy {
+export class EditComponent implements OnInit, OnDestroy, OnChanges {
   @Input() activeClip: IClip | null = null;
+
+  clipID = new FormControl('');
+  title = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ]);
+  editForm = new FormGroup({
+    title: this.title,
+    id: this.clipID
+  });
 
   constructor(
     private modal: ModalService
@@ -21,5 +32,14 @@ export class EditComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.modal.unregister('editClip');
+  }
+
+  ngOnChanges() {
+    if (!this.activeClip) {
+      return;
+    }
+
+    this.clipID.setValue(this.activeClip.docID);
+    this.title.setValue(this.activeClip.title);
   }
 }
