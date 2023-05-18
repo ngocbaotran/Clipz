4d7a05d2a1b9b0a4fc654ead6d90187227de62af
Bao Tran <ngocbao8994@gmail.com>
2023-05-19 00:19:27 +0700

    Querying for Clips

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -12,7 +12,9 @@ import IClip from '../models/clip.model';
   providedIn: 'root'
 })
 export class ClipService {
-  public clipsCollection: AngularFirestoreCollection<IClip>
+  public clipsCollection: AngularFirestoreCollection<IClip>;
+  pageClips: IClip[] = [];
+  pendingReq = false;
 
   constructor(
     private db: AngularFirestore,
@@ -65,5 +67,38 @@ export class ClipService {
     await clipRef.delete();
     await screenshotRef.delete();
     await this.clipsCollection.doc(clip.docID).delete();
+  }
+
+  async getClips() {
+    if (this.pendingReq) {
+      return;
+    }
+
+    this.pendingReq = true;
+    let query = this.clipsCollection.ref
+      .orderBy('timestamp', 'desc')
+      .limit(6);
+    const { length } = this.pageClips;
+
+    if (length) {
+      const lastDocID = this.pageClips[length - 1].docID;
+      const lastDoc = await this.clipsCollection
+        .doc(lastDocID)
+        .get()
+        .toPromise();
+
+      query = query.startAfter(lastDoc);
+    }
+
+    const snapshot = await query.get();
+
+    snapshot.forEach(doc => {
+      this.pageClips.push({
+        docID: doc.id,
+        ...doc.data()
+      });
+    });
+
+    this.pendingReq = false;
   }
 }
