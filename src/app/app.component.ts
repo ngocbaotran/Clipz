6fa190cfc0fb4e2afc2bd6c7d6e31b3d91242eb7
Bao Tran <bao.tran@sstechvn.com>
2023-05-24 16:08:22 +0700

    Add Back to Top Button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from "./services/auth.service";
 
 @Component({
@@ -6,9 +6,23 @@ import { AuthService } from "./services/auth.service";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  windowScrolled = false;
+
   constructor(
     public auth: AuthService
   ) {
+  }
+
+  ngOnInit() {
+    window.addEventListener('scroll', () => {
+      const { scrollTop, offsetHeight } = document.documentElement;
+      const { innerHeight } = window;
+      this.windowScrolled = Math.round(scrollTop) + innerHeight > (offsetHeight / 2);
+    });
+  }
+
+  scrollToTop() {
+    window.scrollTo(0, 0);
   }
 }
